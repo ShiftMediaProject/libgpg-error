2b82d8a85edfd8853f24d06f31a09fbc53bf4a86
Matt Oliver <protogonoi@gmail.com>
2015-08-28 00:46:24 +1000

    project: Update for 1.20 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,8 +1,8 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.19"
+#define PACKAGE_VERSION "1.20"
 
 /* GIT commit id revision used to build this package */
-#define BUILD_REVISION "395b63a"
+#define BUILD_REVISION "954c1b4"
 
 /* The time this package was configured for a build */
-#define BUILD_TIMESTAMP "2015-04-23T06:13+0000"
+#define BUILD_TIMESTAMP "2015-08-27T12:32+0000"
