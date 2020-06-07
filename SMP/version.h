24fde911fadb36e2577472f03be296081d6a1497
Matt Oliver <protogonoi@gmail.com>
2020-06-07 23:39:17 +1000

    project: Update for 1.38 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,8 +1,8 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.37"
+#define PACKAGE_VERSION "1.38"
 
 /* GIT commit id revision used to build this package */
-#define BUILD_REVISION "44fde99"
+#define BUILD_REVISION "373857f"
 
 /* The time this package was configured for a build */
-#define BUILD_TIMESTAMP "2020-02-15T16:00+0000"
+#define BUILD_TIMESTAMP "2020-06-07T12:27+0000"
