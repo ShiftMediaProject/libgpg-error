883ec429ff9e1c1597aa815554b13cd28fe4c41a
Matt Oliver <protogonoi@gmail.com>
2024-06-29 23:54:51 +1000

    project: Update for 1.50 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,8 +1,8 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.49"
+#define PACKAGE_VERSION "1.50"
 
 /* GIT commit id revision used to build this package */
-#define BUILD_REVISION "5822584"
+#define BUILD_REVISION "3206903"
 
 /* The time this package was configured for a build */
-#define BUILD_TIMESTAMP "2024-06-08T13:39+0000"
+#define BUILD_TIMESTAMP "2024-06-29T11:20+0000"
